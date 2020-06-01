3336ab54aae1afd9227d163ff958f0667d810df2
Kevin.K <kevinkusky@gmail.com>
2020-06-01 19:14:47 -0400

    backend auth and basic front end

---
+++ frontend/components/root.jsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import { Provider } from 'react-redux';
+// import { HashRouter } from 'react-router-dom';
+
+// import App from './app';
+
+
+const Root = () => (
+    // <Provider >
+    //     <HashRouter>
+    //         <App />
+    //     </HashRouter>
+    // </Provider>
+    <h3>Would you consider this reactionary?</h3>
+);
+
+export default Root;
