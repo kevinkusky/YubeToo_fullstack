7fb9104020a929ee7b161083b0acb28978f57af2
Kevin.K <kevinkusky@gmail.com>
2020-06-02 14:13:02 -0400

    reboot machine

--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
 
 class Signup extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+        };
+    }
     render (){
-        return null;
+        return(
+            <div className="session-form">
+                <form action="">
+                    <h3>Sign Up!</h3>
+                    <label>Email:
+                        <input type="text"
+                            value={this.state.email}
+                            onChange={this.handleInput('email')}
+                        />
+                    </label>
+                    <label>Password:
+                        <input type="password"
+                            value={this.state.password}
+                            onChange={this.handleInput('password')}
+                        />
+                    </label>
+                    <input type="submit"
+                    />
+                </form>
+            </div>
+        )
     }
 }
 
