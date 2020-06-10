cc8a4bf7c379b0203d3de01909342641f5063313
Kevin.K <kevinkusky@gmail.com>
2020-06-09 21:53:24 -0400

    video model, view, controller created

---
+++ app/controllers/api/videos_controller.rb
@@ -0,0 +1,19 @@
+class VideosController < ApplicationController
+    before_action :ensure_logged_in, only: [:create, :destroy]
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def create
+
+    end
+
+    def destroy
+
+    end
+end
