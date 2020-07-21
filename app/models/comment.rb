5f91ad3b43a636d3f1253ff25880ba7c07ffe409
Kevin.K <kevinkusky@gmail.com>
2020-07-21 13:52:44 -0400

    Migrates comments table with annotates

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id                :bigint           not null, primary key
+#  body              :string           not null
+#  video_id          :integer          not null
+#  author_id         :integer          not null
+#  parent_comment_id :integer
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
 class Comment < ApplicationRecord
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@
 #                           POST   /api/session(.:format)                                                                   api/sessions#create {:format=>:json}
 #                 api_users POST   /api/users(.:format)                                                                     api/users#create {:format=>:json}
 #                  api_user GET    /api/users/:id(.:format)                                                                 api/users#show {:format=>:json}
+#                           DELETE /api/users/:id(.:format)                                                                 api/users#destroy {:format=>:json}
+#                api_videos GET    /api/videos(.:format)                                                                    api/videos#index {:format=>:json}
+#                           POST   /api/videos(.:format)                                                                    api/videos#create {:format=>:json}
+#                 api_video GET    /api/videos/:id(.:format)                                                                api/videos#show {:format=>:json}
+#                           PATCH  /api/videos/:id(.:format)                                                                api/videos#update {:format=>:json}
+#                           PUT    /api/videos/:id(.:format)                                                                api/videos#update {:format=>:json}
+#                           DELETE /api/videos/:id(.:format)                                                                api/videos#destroy {:format=>:json}
 #        rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
 # rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
 #        rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
