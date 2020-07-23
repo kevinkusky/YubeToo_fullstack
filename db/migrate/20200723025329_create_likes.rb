55664fb993b01490b12cebad76710d9b23c27ec9
Kevin.K <kevinkusky@gmail.com>
2020-07-22 23:05:24 -0400

    creates polymorphic likes table

---
+++ db/migrate/20200723025329_create_likes.rb
@@ -0,0 +1,11 @@
+class CreateLikes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :likes do |t|
+
+        t.references :likeable, polymorphic: true
+        t.integer :liker_id, null: false
+        
+      t.timestamps
+    end
+  end
+end
