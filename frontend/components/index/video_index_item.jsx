aba32bdaf0ff36032a579eb16cb63a71d69dc8fe
Kevin.K <kevinkusky@gmail.com>
2020-07-24 16:28:18 -0400

    Adds final class names

--- frontend/components/index/video_index_item.jsx
+++ frontend/components/index/video_index_item.jsx
@@ -16,18 +16,18 @@ class VideoIndexItem extends React.Component {
         const {title, title_card_url} = this.props.video;
         return(
             <div className='index-preview' onClick={this.handleClick}>
-                <div>
-                    <img src={title_card_url}/>
-                    <div>
-                        <span className='video-title'>{title}</span>
-                        <ul className='secondary-info'>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                        </ul>
-                        <span className='video-discription'></span>
-
-                    </div>
+                <img 
+                    className='preview-image' 
+                    src={title_card_url}
+                />
+                <div className='preview-information'>
+                    <span className='video-title'>{title}</span>
+                    <ul className='secondary-info'>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <span className='video-discription'></span>
                 </div>
             </div>
         )
