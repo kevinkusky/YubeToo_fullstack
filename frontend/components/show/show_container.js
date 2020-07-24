89c01b0a71b4584ac265d8008b802346ccb58966
Kevin.K <kevinkusky@gmail.com>
2020-07-24 16:12:03 -0400

    Structures show container

---
+++ frontend/components/show/show_container.js
@@ -0,0 +1,12 @@
+// import {connect} from 'react-redux';
+// import VideoShow from './show';
+
+// // const mSTP = state => ({
+// //     video
+// // });
+
+// // const mDTP = dispatch => ({
+
+// // });
+
+// export default connect(null)(VideoShow);
