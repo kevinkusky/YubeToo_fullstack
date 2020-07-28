ea4301117093bec40e66cf8b40dce33907f5756a
Kevin.K <kevinkusky@gmail.com>
2020-07-28 14:28:20 -0400

    video index

--- frontend/components/index/video_index.jsx
+++ frontend/components/index/video_index.jsx
@@ -5,7 +5,7 @@ import VideoIndexItem from './video_index_item';
 const VideoIndex = ({ videos })=> {
     return(
         <div>
-            <h2>All Recomendations</h2>
+            <h2 className='index-header'>All Recomendations</h2>
             {videos.map(video =>(
                 <VideoIndexItem
                     video={video}
