93aac7774cebe67565ec6e9389f8822caf2be243
Kevin.K <kevinkusky@gmail.com>
2020-08-01 01:20:05 -0400

    quick commit

--- frontend/components/navs/topnav.jsx
+++ frontend/components/navs/topnav.jsx
@@ -2,12 +2,14 @@ import {Link} from 'react-router-dom';
 import React from 'react';
 import { connect } from 'react-redux';
 import GreetingContainer from '../greeting/greeting_container';
+import MenuIcon from '@material-ui/icons/Menu';
 
  const TopNav = () =>{
     return(
         <header className='nav-bar'>
             <nav>
                 <ul className='nav-left'>
+                    <li><MenuIcon /></li>
                     <Link to='/' className='header-link'>
                         <li><h3 className='logo'>{"▷"}</h3></li>
                         <li><h3 className='site-name'>YubeToo</h3></li>
