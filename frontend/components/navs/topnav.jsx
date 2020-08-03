26dbd6a744d2581e9860f8a294ef690ebb459805
Kevin.K <kevinkusky@gmail.com>
2020-08-03 13:17:54 -0400

    Fixes top nav code

--- frontend/components/navs/topnav.jsx
+++ frontend/components/navs/topnav.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
 import GreetingContainer from './greeting_container';
 import LeftNav from './leftnav';
 import SearchBar from './searchbar';
+import { LAUTH, SUPAUTH } from '../../util/route_utils';
 
-const TopNav = () => {
+class TopNav extends React.Component {
     // turnary statement to check
     // this.props.location.pathname
     // create util consts for paths
 
-    const navBar = () => (
-        <div className='header'>
-            <nav className='nav-bar'>
-                <LeftNav />
-                <SearchBar />
-                <GreetingContainer />
-            </nav>
-        </div>
-    );
-    
-    const noBar = () => (
-        <div></div>
-    )
-    
+    constructor(props) {
+        super(props);
+    }
 
+    navBar() {
+        return(
+            <div className='header'>
+                <nav className='nav-bar'>
+                    <LeftNav />
+                    <SearchBar />
+                    <GreetingContainer />
+                </nav>
+            </div>
+        )
+    }
+    
+    noBar() {return(<div></div>)}
+
+    render() {
+        return (this.props.location.pathname === LAUTH ||
+            this.props.location.pathname === SUPAUTH) ? this.noBar() : this.navBar()
+    }
 };
 
-export default connect(null)(TopNav);
+export default withRouter(connect(null)(TopNav));
