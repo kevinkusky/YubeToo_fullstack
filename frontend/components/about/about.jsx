b6d057a86e84a1a27c7a8650824b8f34fb68c56f
Kevin.K <kevinkusky@gmail.com>
2020-08-08 13:03:43 -0400

    Adds active links to about me list

--- frontend/components/about/about.jsx
+++ frontend/components/about/about.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import GithubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
-// import PortfolioIcon from '@material-ui/icons/';
+import PortfolioIcon from '@material-ui/icons/PersonOutline';
 import EmailIcon from '@material-ui/icons/EmailOutlined';
 
 const About = () => (
@@ -17,15 +17,22 @@ const About = () => (
             <div className='portfoliolinks'>
                 <ul>
                     <li>
+                        <a target='_blank' href="https://kevinkusky.github.io/"><PortfolioIcon /></a>
+                    </li>
+                    <li>
                         <a target='_blank' href="https://www.linkedin.com/in/kevinkusky/"><LinkedInIcon /></a>
                     </li>
-                    <li><GithubIcon /></li>
+                    <li>
+                        <a target='_blank' href="https://github.com/kevinkusky"><GithubIcon /></a>
+                    </li>
                     <li><EmailIcon />: KevinKusky@gmail.com</li>
                 </ul>
             </div>
         </div>
         <div className='about-bottom'>
-            <p></p>
+            <p>Hello, I am a bio that will be filled out shortly with words - such very cool,
+                awesome, persuasive words that will make you hire me.
+            </p>
         </div>
     </div>
 );
