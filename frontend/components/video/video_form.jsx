795404af1f3c940aa4d0736b73a8b414c521c5df
Kevin.K <kevinkusky@gmail.com>
2020-08-16 21:06:58 -0400

    dropzone file handleing

--- frontend/components/video/video_form.jsx
+++ frontend/components/video/video_form.jsx
@@ -6,134 +6,161 @@ import TopNav from '../navs/topnav';
 
 import VideoDrop from '@material-ui/icons/PublishOutlined';
 
-class VideoForm extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            title: '',
-            description: '',
-            creator_id: this.props.currentUser.id,
-            videoFile: null,
-            videoUrl: null,
-            titlecardFile: null,
-            titlecardUrl: null
-        };
-        this.handleFile = this.handleFile.bind(this);
-        this.handlePicFile = this.handlePicFile.bind(this);
-        this.navigateToSplash = this.navigateToSplash.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+class VideoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      description: "",
+      creator_id: this.props.currentUser.id,
+      errors: '',
+      videoFile: null,
+      videoUrl: null,
+      titlecardFile: null,
+      titlecardUrl: null,
+    };
+    // this.handleFile = this.handleFile.bind(this);
+    this.handlePicFile = this.handlePicFile.bind(this);
+    this.navigateToSplash = this.navigateToSplash.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleVideoDrop = this.handleVideoDrop.bind(this);
+  }
+
+  navigateToSplash() {
+    // refactor to profile page if/when built
+    this.props.history.push("/");
+  }
+
+  update(field) {
+    return (e) => this.setState({ [field]: e.currentTarget.value });
+  }
+
+  handlePicFile(e) {
+    const file = e.currentTarget.files[0];
+    const fileReader = new FileReader();
+
+    fileReader.onloadend = () => {
+      this.setState({ titlecardFile: file, titlecardUrl: fileReader.result });
+    };
+
+    if (file) {
+      fileReader.readAsDataURL(file);
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("video[title]", this.state.title);
+    formData.append("video[description]", this.state.description);
+    formData.append("video[creator_id]", this.state.creator_id);
+
+    if (this.state.videoFile) {
+      formData.append("video[video]", this.state.videoFile);
+    }
+    if (this.state.titlecardFile) {
+      formData.append("video[titlecard]", this.state.titlecardFile);
     }
 
-    navigateToSplash(){
-        // refactor to profile page if/when built
-        this.props.history.push('/');
-    }
+    this.props.createVideo(formData);
+  }
 
-    update(field){
-        return e => this.setState({ [field]: e.currentTarget.value });
-    }
+  // current file handler for videos
+//   handleFile(e) {
+//     const file = e.currentTarget.files[0];
+//     const fileReader = new FileReader();
 
-    handlePicFile(e){
-        const file = e.currentTarget.files[0];
-        const fileReader = new FileReader();
-        
+//     fileReader.onloadend = () => {
+//       this.setState({ videoFile: file, videoUrl: fileReader.result });
+//     };
+
+//     if (file) {
+//       fileReader.readAsDataURL(file);
+//     }
+//   }
+
+  // dropzonehandler
+
+  handleVideoDrop(videoFiles) {
+    //update videoFiles array as user drag videoFiles to drop zone
+    if (videoFiles) {
+      //set drop zone error to empty if there is any error
+      if (this.state.errors.length !== 0) {
+        this.setState({ errors: "" });
+      }
+
+      for (let idx = 0; idx < videoFiles.length; idx++) {
+        let fileReader = new FileReader();
+        const videoFile = videoFiles[idx];
+
         fileReader.onloadend = () => {
-            this.setState({ titlecardFile: file, titlecardUrl: fileReader.result });
+          if (this.state.videoFile) {
+            //update photoFiles and photoUrls in state
+            this.setState({
+              photoFiles: this.state.videoFile.concat(
+                URL.createObjectURL(videoFile)
+              ),
+              videoUrls: this.state.videoUrl.concat(videoFile),
+            });
+          } else {
+            //set drop zone error in state
+            this.setState({ errors: "May only upload 1 video at a time" });
+          }
         };
-        
-        if(file){
-            fileReader.readAsDataURL(file);
-        }
+        fileReader.readAsDataURL(videoFile);
+      }
     }
-    
-    handleSubmit(e) {
-        e.preventDefault();
-        
-        const formData = new FormData();
-        formData.append('video[title]', this.state.title);
-        formData.append('video[description]', this.state.description);
-        formData.append('video[creator_id]', this.state.creator_id);
-        
-        if(this.state.videoFile){
-            formData.append('video[video]', this.state.videoFile);
-        }
-        if(this.state.titlecardFile){
-            formData.append('video[titlecard]', this.state.titlecardFile);
-        }
-        
-        this.props.createVideo(formData);
-    }
-    
-    // current file handler for videos
-    handleFile(e){
-        const file = e.currentTarget.files[0];
-        const fileReader = new FileReader();
+  }
 
-        fileReader.onloadend = () => {
-            this.setState({ videoFile: file, videoUrl: fileReader.result });
-        };
-
-        if(file){
-            fileReader.readAsDataURL(file);
-        }
-    }
-
-    // dropzonehandler
-
-    // handleDrop(videoFile){
-    //     if(videoFile){
-    //         if(this.state.errors.length !== 0){
-    //             this.setState({errors: ''});
-    //         }
-    //     }
-    // }
-
-    render(){
-        // refactor preview to be a title card upload
-        const preview = this.state.titlecardUrl ? <img height='200px' width='200px' src={this.state.titlecardUrl}/> : null;
-        return (
-            <div>
-                <TopNav />
-                <form onSubmit={this.handleSubmit}>
-                    <h3>Upload Video</h3>
-                    <Dropzone onDrop={this.handleFile}>
-                        {({ getRootProps, getInputProps }) => (
-                            <div {...getRootProps({className: 'drop-zone'})}>
-                                <input {...getInputProps()}/>
-                                <div className='dropzone-target'>
-                                    <p>test for drop zone</p>
-                                </div>
-                            </div>
-                        )}
-                    </Dropzone>
-                    {/* <input type="file" onChange={this.handleFile}/> */}
-                    <h3>Upload Preview Image</h3>
-                    <input type="file" onChange={this.handlePicFile}/>
-                <br/>
-                    <h4>Preview</h4>
-                    {preview}
-                <br/>
-                <br/>
-                    <input 
-                        type="text"
-                        value={this.state.title}
-                        onChange={this.update('title')}
-                        required=' '
-                    />
-                    <label className='upload-labels'>Title</label>
-                <br/>
-                    <input 
-                        type="text"
-                        value={this.state.description}
-                        onChange={this.update('description')}
-                    />
-                    <label className='upload-labels'>Description</label>
-                <br/>
-                    <input type="submit" value='Submit'/>
-                </form>
-            </div>
-        );
-    }
+  render() {
+    // refactor preview to be a title card upload
+    const preview = this.state.titlecardUrl ? (
+      <img height="200px" width="200px" src={this.state.titlecardUrl} />
+    ) : null;
+    return (
+      <div>
+        <TopNav />
+        <form onSubmit={this.handleSubmit}>
+          <h3>Upload Video</h3>
+          <Dropzone onDrop={this.handleVideoDrop}>
+            {({ getRootProps, getInputProps }) => (
+              <div {...getRootProps({ className: "drop-zone" })}>
+                <input {...getInputProps()} />
+                <div className="dropzone-target">
+                  <p>test for drop zone</p>
+                </div>
+              </div>
+            )}
+          </Dropzone>
+          {/* <input type="file" onChange={this.handleFile}/> */}
+          <h3>Upload Preview Image</h3>
+          <input type="file" onChange={this.handlePicFile} />
+          <br />
+          <h4>Preview</h4>
+          {preview}
+          <br />
+          <br />
+          <input
+            type="text"
+            value={this.state.title}
+            onChange={this.update("title")}
+            required=" "
+          />
+          <label className="upload-labels">Title</label>
+          <br />
+          <input
+            type="text"
+            value={this.state.description}
+            onChange={this.update("description")}
+          />
+          <label className="upload-labels">Description</label>
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default withRouter(VideoForm);
