332de4f264bc0655c23d593d72bd7d43d903e56b
Kevin.K <kevinkusky@gmail.com>
2020-08-24 23:12:16 -0400

    clean up

--- frontend/components/about/about.jsx
+++ frontend/components/about/about.jsx
@@ -1,37 +1,59 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 import GithubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import PortfolioIcon from '@material-ui/icons/PersonOutline';
 import EmailIcon from '@material-ui/icons/EmailOutlined';
 
-const About = () => (
-    <div className='about-me'>
-        <div className='about-top'>
-            <div className='portfolio-image'>
-                <img className='portfolio-image'
-                    src={window.heroURL}
-                />
+class About extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    // componentDidMount(){ 
+    //     this.copyText();
+    // }
+
+    // copyText(){
+    //     let email = document.getElementsByClassName('about-email')[0].value;
+
+    //     email.select();
+    //     document.execCommand('copy');
+    // }
+
+    render(){
+        return(
+            <div className='about-me'>
+                <div className='about-top'>
+                    <div className='portfolio-image'>
+                        <img className='portfolio-image' src={window.heroURL}/>
+                    </div>
+                    <div className='portfoliolinks'>
+                        <div className='about-link-item'>
+                            <a target='_blank' href="https://kevinkusky.github.io/"><PortfolioIcon className='about-icon'/></a>
+                        </div>
+                        <div className='about-link-item'>
+                            <a target='_blank' href="https://www.linkedin.com/in/kevinkusky/"><LinkedInIcon className='about-icon'/></a>
+                        </div>
+                        <div className='about-link-item'>
+                            <a target='_blank' href="https://github.com/kevinkusky"><GithubIcon className='about-icon'/></a>
+                        </div>
+                        <div className='about-link-item'>
+                            <EmailIcon className='about-icon' 
+                                // onClick={this.copyText()}
+                            />
+                            <input type='text' value='KevinKusky@gmail.com' className='about-email'></input>
+                        </div>
+                    </div>
+                </div>
+                <div className='about-bottom'>
+                    <p className='about-paragraph'>Hello, I am a bio that will be filled out shortly with words - such very cool,
+                        awesome, persuasive words that will make you hire me.
+                    </p>
+                </div>
             </div>
-            <div className='portfoliolinks'>
-                <div className='about-link-item'>
-                    <a target='_blank' href="https://kevinkusky.github.io/"><PortfolioIcon className='about-icon'/></a>
-                </div>
-                <div className='about-link-item'>
-                    <a target='_blank' href="https://www.linkedin.com/in/kevinkusky/"><LinkedInIcon className='about-icon'/></a>
-                </div>
-                <div className='about-link-item'>
-                    <a target='_blank' href="https://github.com/kevinkusky"><GithubIcon className='about-icon'/></a>
-                </div>
-                <div className='about-link-item'><EmailIcon className='about-icon'/><span>: KevinKusky@gmail.com</span></div>
-            </div>
-        </div>
-        <div className='about-bottom'>
-            <p className='about-paragraph'>Hello, I am a bio that will be filled out shortly with words - such very cool,
-                awesome, persuasive words that will make you hire me.
-            </p>
-        </div>
-    </div>
-);
+        )
+    }
+}
 
 export default About;
