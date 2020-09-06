0d3d594478d6e89e339903791289b0d1ea1e9f92
Kevin.K <kevinkusky@gmail.com>
2020-09-06 14:21:11 -0400

    shell of history index

---
+++ frontend/components/index/history_index.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { connect } from "react-redux";
+import { videosAsArray } from "../../reducers/selectors";
+import VideoIndexItem from "./video_index_item";
+
+class UserIndex extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: this.props.videos,
+    };
+  }
+
+  componentDidUpdate(preProps, preState) {
+    if (preProps.videos.length !== this.props.videos.length) {
+      this.setState({ videos: this.props.videos });
+    }
+  }
+
+  render() {
+    return (
+      <div className="videos-index">
+        <h2 className="index-header">My Videos</h2>
+        <div className="videos-list">
+          {this.state.videos.map((video) => (
+            <VideoIndexItem
+              video={video}
+              key={video.id}
+              user={this.props.currentUser}
+              creator={this.props.creator}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+const mSTP = (state) => ({
+  currentUser: state.session.currentUser ? state.session.currentUser.id : null,
+  creator: state.session.currentUser.username,
+  videos: videosAsArray(state.session.currentUser.videosViewed),
+});
+
+export default connect(mSTP)(UserIndex);
