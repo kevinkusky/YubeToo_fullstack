48e7478fc9cd15f9658da74a53190204570aae1f
Kevin.K <kevinkusky@gmail.com>
2020-10-27 21:32:34 -0400

    plugs in trending index

--- frontend/components/index/trending_index.jsx
+++ frontend/components/index/trending_index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from "react-redux";
-import { videosAsArray } from "../../reducers/selectors";
+import { entityAsArray } from "../../reducers/selectors";
 import { fetchVideos } from "../../actions/videos";
 import VideoIndexItem from './video_index_item';
 
@@ -8,59 +8,43 @@ class TrendingIndex extends React.Component {
     constructor(props) {
         super(props);
 
-        // this.state = {
-        //     popIndex: this.indexSort(this.props.videos),
-        //     tempArr: []
-        // };
-        // this.indexSort = this.indexSort.bind(this);
+        this.state = {
+            popIndex: [],
+        };
+        this.indexSort = this.indexSort.bind(this);
+    }
+
+    componentDidMount(){
+        this.indexSort(this.props.videos);
     }
 
 
-    // indexSort(videoList){
-    //     if (this.state.popIndex.length < 2 ) return videoList;
-    //     const sort = (x, y) => (x < y ? -1 : 1);
-
-    //     const pivot = videoList[0];
-
-    //     let left = videoList.slice(1).filter((video) => 
-    //         sort(video.props.viewCount, pivot.props.viewCount) === -1)
-    //     ;
-    //     let right = videoList.slice(1).filter((video) =>
-    //         sort(video.props.viewCount, pivot.props.viewCount) !== -1)
-    //     ;
-
-    //     left = this.indexSort(left);
-    //     right = this.indexSort(right);
-
-    //     this.setState({popIndex: left.concat([pivot]).concat(right)});
-    // }
+    indexSort(videoList){
+        let sortedList = videoList.sort((a,b) => (a.views > b.views) ? -1 : 1);
+        this.setState({ popIndex: sortedList });
+    }
 
     render() {
         return (
             <div>
                 <h2 className='index-header'>Whats Hot</h2>
                 <div className='videos-list'>
-                    {/* {this.state.popIndex.map(video => (
+                    {this.state.popIndex.map(video => (
                         <VideoIndexItem
                             video={video}
                             key={video.id}
                             user={this.props.currentUser}
                         />
-                    ))} */}
+                    ))}
                 </div>
             </div>
         )
     }
 }
 
-// const mSTP = (state) => ({
-//     currentUser: state.session.currentUser ? state.session.currentUser.id : null,
-//     videos: videosAsArray(state.entities.videos),
-// });
+const mSTP = (state) => ({
+    currentUser: state.session.currentUser ? state.session.currentUser.id : null,
+    videos: entityAsArray(state.entities.videos),
+});
 
-// const mDTP = (dispatch) => ({
-//     fetchVideos: () => dispatch(fetchVideos()),
-// });
-
-// export default connect(mSTP, mDTP)(TrendingIndex);
-export default connect(null)(TrendingIndex);
+export default connect(mSTP)(TrendingIndex);
